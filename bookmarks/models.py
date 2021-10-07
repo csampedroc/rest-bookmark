b440449d9516cf799e10960152c030277fe4b665
Carlos Sampedro <carlos.sampedroc@gmail.com>
2021-10-07 13:02:35 -0500

    feat: :construction: Added created_by field in bookmark model

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -6,5 +6,6 @@ class Bookmark(models.Model):
     title = models.CharField(max_length=200)
     url = models.URLField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey('auth.User', related_name='bookmarks', on_delete=models.CASCADE)
     class Meta:
         ordering = ['created_at']
