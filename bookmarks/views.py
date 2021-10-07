27ff9dad636a0bb09d244c60b67b4309e9bc1ea5
Carlos Sampedro <carlos.sampedroc@gmail.com>
2021-10-07 13:06:47 -0500

    feat: :construction: Added views

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics, permissions
 
-# Create your views here.
+from .models import Bookmark
+from .permissions import IsOwnerOrReadOnly
+from .serializers import BookmarkSerializer, UserSerializer
+
+
+class BookmarkList(generics.ListCreateAPIView):
+    queryset = Bookmark.objects.all()
+    serializer_class = BookmarkSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]    
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)    
+
+
+class BookmarkDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Bookmark.objects.all()
+    serializer_class = BookmarkSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
