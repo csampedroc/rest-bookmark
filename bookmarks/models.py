e4c87d24634ccda91bb7d9b68c132b5132b519df
Carlos Sampedro <carlos.sampedroc@gmail.com>
2021-10-07 12:00:21 -0500

    feat: :construction: Added Bookmark model

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Bookmark(models.Model):
+    title = models.CharField(max_length=200)
+    url = models.URLField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
